69b3e1d671b29e4964e8b08ff5be3e40dad02dae
Justin <83477376+JustinFire@users.noreply.github.com>
2021-11-03 20:15:55 -0400

    Create  CCC '96 S2 - Divisibility by 11.cpp

---
+++ 1996/CCC '96 S2 - Divisibility by 11.cpp
@@ -0,0 +1,65 @@
+// Have not verified through DMOJ. Verified using test cases. Will verifiy through DMOJ when they fix submissions
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#define ll long long
+#define pi 3.14159265359
+#define yes std::cout << "YES" << endl
+#define no  std::cout << "NO" << endl
+
+
+using namespace std;
+
+void solve();
+
+
+
+
+int main() {
+    solve();
+
+    return 0;
+}
+
+void solve() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+
+    cin >> t;
+
+
+    for (int i = 0; i < t; i++) {
+        unsigned long long num;
+        cin >> num;
+
+        string og = to_string(num);
+
+        cout << num << "\n";
+        for (int i = 0; i < INT_MAX; i++) {
+            int past;
+            if (to_string(num).length() == 2) {
+                if (num % 11 == 0) {
+                    cout << "The number " << og << " is divisible by 11.\n\n";
+                    break;
+                }
+                cout << "The number " << og << " is not divisible by 11.\n\n";
+                break;
+
+            }
+            past = num % 10;
+            num = num / 10;
+            num = num - past;
+            cout << num << "\n";
+
+        }
+
+    }
+
+}
